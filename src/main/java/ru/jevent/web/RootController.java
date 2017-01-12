b4f6a610726c74e150bb781c3d82f9a6850f6264
Alex Ivanov <alsi.exp@gmail.com>
2017-01-12 18:44:31 +0300

    Add common RootController

---
+++ src/main/java/ru/jevent/web/RootController.java
@@ -0,0 +1,27 @@
+package ru.jevent.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class RootController {
+
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public String userList() {
+        return "userList";
+    }
+
+    @RequestMapping(value = "/participants", method = RequestMethod.GET)
+    public String participantList() {
+        return "participantList";
+    }
+
+    @RequestMapping(value = "/speaker/{speakerId}", method = RequestMethod.GET)
+    public String speaker(@PathVariable String speakerId, Model model) {
+        model.addAttribute("speakerId", speakerId);
+        return "speaker";
+    }
+}
